43d6c9754fa859890430bd8c5de64d7ca088d5bd
Walker Bagley <mathguy2003@gmail.com>
2021-07-14 13:50:32 -0700

    Initial Gallery Grid, tag mapping

---
+++ components/Entry.js
@@ -0,0 +1,44 @@
+export default function Entry({ metadata, tags, url }) {
+
+    let tagList = Object.values(tags);
+    let classList = "container";
+    for (const tag of tagList) {
+        classList += " ";
+        classList += String(tag);
+        console.log(classList);
+    };
+
+    return (
+        <>
+            <div className={classList}>
+                <div className="image"><img src={url} alt="" /></div>
+                <div className="data">
+                    <div className="metadata">
+                        <div className="entry">{metadata[0]}</div>
+                        <div className="entry">{metadata[1]}</div>
+                        <div className="entry">{metadata[2]}</div>
+                        <div className="entry">{metadata[3]}</div>
+                        <div className="entry">{metadata[4]}</div>
+                    </div>
+                    <div className="other">
+                        <div className="date">{metadata[5]}</div>
+                        <div className="location">{metadata[6]}</div>
+                    </div>
+                </div>
+            </div>
+            <style jsx global>{`
+                .container{
+                    width: 100%;
+                    height: auto;
+                }
+                .image img{
+                    width: 100%;
+                    height: auto;
+                }
+                .data{
+                    display: none;
+                }
+            `}</style>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import Entry from '../components/Entry'
+
 export default function Home() {
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			<div className="grid">
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+				<Entry metadata={{ "0": "Single Shot", "1": "30mm", "2": "1/1250\"", "3": "f/2", "4": "ISO 100", "5": "17 Oct 2020", "6": "Moon Valley, AZ" }} tags={{ "0": "placeholder1", "1": "placeholder2" }} url="https://drive.google.com/uc?export=view&id=1NzvR57wR9Bz3uq7tS943w6q7DkqUPDUq" />
+			</div>
+
+			<style jsx global>{`
+				.grid{
+					padding: 5em 10vw;
+					display: grid;
+					grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+					grid-auto-rows: 1fr;
+					gap: 1em;
+				}
+      		`}</style>
+		</>
+	)
 }
