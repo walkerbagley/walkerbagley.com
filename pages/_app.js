9d81240b8e2b2425cfb8cef02162a422f75fac12
Walker Bagley <mathguy2003@gmail.com>
2021-07-16 01:48:20 -0700

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,25 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export default function CustomApp({ Component, pageProps }) {
-  return <>
-      <Head>
-        <title>Walker Bagley</title>
-        <meta name="description" content="Walker Bagley's Photography Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-	  <Navbar/>
-      <Component {...pageProps}/>
-	  <Footer/>
-    </>
+	return <>
+		<Head>
+			<title>Walker Bagley</title>
+			<meta name="description" content="Walker Bagley's Photography Portfolio" />
+			<link rel="icon" href="/favicon.ico" />
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-0QDS47PL1L"></script>
+			<script>
+				dangerouslySetInnerHTML={{
+					__html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', 'G-0QDS47PL1L');
+				`,
+				}}
+			</script>
+		</Head>
+		<Navbar />
+		<Component {...pageProps} />
+		<Footer />
+	</>
 }
